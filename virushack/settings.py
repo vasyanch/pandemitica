ac19b3e836abf1c72f8e9d3e1ebc8e4dd2f4321f
v.mikhaylov <v.mikhaylov@adventum.ru>
2020-05-04 14:26:32 +0300

    static

--- virushack/settings.py
+++ virushack/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,8 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = 'static'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
@@ -135,3 +135,6 @@ GOOGLE_STORAGE_CRED = \
     )
 
 GOOGLE_BUCKET_NAME = 'virushack_pandemitica'
+
+django_heroku.settings(locals())
+
